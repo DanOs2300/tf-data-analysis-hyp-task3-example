77b03ab3d75755fdb83e99a4af422073468c28f0
DanOs2300 <87863552+DanOs2300@users.noreply.github.com>
2023-04-21 23:22:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-
+from statsmodels.stats.weightstats import ztest
 
 chat_id = 630197911 # Ваш chat ID, не меняйте название переменной
 
-def solution(x, y):
-    result = ttest_ind(x, y, equal_var=False, alternative='two-sided')
-    return result.pvalue < 0.03
+def solution(x):
+    result = ztest(x1=x, value=500, alternative='larger')
+    return result[1] < 0.06
